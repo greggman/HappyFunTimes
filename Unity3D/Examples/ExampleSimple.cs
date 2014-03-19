58aead9a031f9250fdcb25cae2e9a37854306842
Gregg Tavares <github@greggman.com>
2014-03-20 01:16:41 +0900

    Add Unity3D files

---
+++ Unity3D/Examples/ExampleSimple.cs
@@ -0,0 +1,233 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using HappyFunTimes;
+using CSSParse;
+
+namespace HappyFunTimesExample {
+
+public class ExampleSimple : MonoBehaviour {
+
+    public int areaWidth = 300;  // matches JavaScript
+    public int areaHeight = 300;
+
+    public GameObject prefabToSpawn;
+
+    class Goal {
+        public Goal(ExampleSimple exampleSimple) {
+            m_exampleSimple = exampleSimple;
+            m_position = new Vector3();
+            m_color = new Color();
+            m_gameObject = (GameObject)Instantiate(m_exampleSimple.prefabToSpawn, Vector3.zero, Quaternion.identity);
+            PickGoal();
+        }
+
+        public void PickGoal() {
+            m_position.x = m_exampleSimple.m_rand.Next(m_exampleSimple.areaWidth);
+            m_position.z = m_exampleSimple.m_rand.Next(m_exampleSimple.areaHeight);
+        }
+
+        public void Update() {
+            if ((Time.frameCount & 4) == 0) {
+                m_color.r = 1.0f;
+                m_color.g = 0.75f;
+                m_color.b = 0.80f;
+            } else {
+                m_color.r = 1.0f;
+                m_color.g = 0.0f;
+                m_color.b = 0.0f;
+            }
+
+            m_gameObject.renderer.material.color = m_color;
+            m_gameObject.transform.localPosition = m_position;
+        }
+
+        public bool IsHit(Vector3 otherPosition) {
+            float dx = otherPosition.x - m_position.x;
+            float dz = otherPosition.z - m_position.z;
+            return dx * dx + dz * dz < m_radiusesSquared;
+        }
+
+        private ExampleSimple m_exampleSimple;
+        private GameObject m_gameObject;
+        private Vector3 m_position;
+        private Color m_color;
+        private float m_radiusesSquared = (15 * 15 * 2);
+    }
+
+    class Player {
+        // Classes based on MessageCmdData are automatically registered for deserialization
+        // by CmdName.
+        [CmdName("color")]
+        public class MessageColor : MessageCmdData {
+            public string color;    // in CSS format rgb(r,g,b)
+        };
+
+        [CmdName("move")]
+        public class MessageMove : MessageCmdData {
+            public int x;
+            public int y;
+        };
+
+        // NOTE: This message is only sent, never received
+        // therefore it does not need a no parameter constructor.
+        // If you do receive one you'll get an error unless you
+        // add a no parameter constructor.
+        [CmdName("scored")]
+        public class MessageScored : MessageCmdData {
+            public MessageScored(int _points) {
+                points = _points;
+            }
+
+            public int points;
+        }
+
+        public Player(NetPlayer netPlayer, ExampleSimple exampleSimple) {
+            m_netPlayer = netPlayer;
+            m_exampleSimple = exampleSimple;
+            m_gameObject = null;
+            m_position = new Vector3(m_exampleSimple.m_rand.Next(m_exampleSimple.areaWidth), 0, m_exampleSimple.m_rand.Next(m_exampleSimple.areaHeight));
+            m_color = new Color(0.0f, 1.0f, 0.0f);
+
+            // Setup events for the different messages.
+            netPlayer.RegisterCmdHandler<MessageColor>(OnColor);
+            netPlayer.RegisterCmdHandler<MessageMove>(OnMove);
+        }
+
+        public void Remove() {
+            Destroy(m_gameObject);
+            m_gameObject = null;
+        }
+
+        public void Update() {
+            // We can't spawn game objects except during Start/Update
+            if (m_gameObject == null) {
+                m_gameObject = (GameObject)Instantiate(m_exampleSimple.prefabToSpawn, Vector3.zero, Quaternion.identity);
+            } else {
+                m_gameObject.renderer.material.color = m_color;
+                m_gameObject.transform.localPosition = m_position;
+            }
+        }
+
+        // Check if this Player is associated with a particular NetPlayer.
+        public bool IsNetPlayer(NetPlayer netPlayer) {
+            return netPlayer == m_netPlayer;
+        }
+
+        private void OnColor(MessageColor data) {
+            m_color = Style.ParseCSSColor(data.color);
+        }
+
+        private void OnMove(MessageMove data) {
+            m_position.x = data.x;
+            m_position.z = m_exampleSimple.areaHeight - data.y - 1;  // because in 2D down is positive.
+
+            if (m_gameObject) {
+                m_gameObject.transform.localPosition = m_position;
+            }
+            if (m_exampleSimple.HitGoal(m_position)) {
+                m_netPlayer.SendCmd(new MessageScored(m_exampleSimple.m_rand.Next(5, 15)));
+            }
+        }
+
+        private NetPlayer m_netPlayer;
+        private ExampleSimple m_exampleSimple;
+        private GameObject m_gameObject;
+        private Vector3 m_position;
+        private Color m_color;
+
+
+        // TODO/FIX move to some utils class
+    }
+
+    class CheckNetPlayer {
+        public CheckNetPlayer(NetPlayer netPlayer) {
+            m_netPlayer = netPlayer;
+        }
+
+        public bool IsNetPlayer(Player player) {
+            return player.IsNetPlayer(m_netPlayer);
+        }
+
+        private NetPlayer m_netPlayer;
+    }
+
+    bool HitGoal(Vector3 position) {
+        bool hit = m_goal.IsHit(position);
+        if (hit) {
+            m_goal.PickGoal();
+        }
+        return hit;
+    }
+
+    // Use this for initialization
+    void Start () {
+        m_rand = new System.Random();
+        m_players = new List<Player>();
+        m_goal = new Goal(this);
+
+        m_server = new GameServer();
+        m_server.Init();
+
+        m_server.OnPlayerConnect += StartNewPlayer;
+        m_server.OnPlayerDisconnect += RemovePlayer;
+        m_server.OnConnect += Connected;
+        m_server.OnDisconnect += Disconnected;
+    }
+
+    void StartNewPlayer(object sender, PlayerConnectMessageArgs e) {
+        m_players.Add(new Player(e.netPlayer, this));
+    }
+
+    void RemovePlayer(object sender, PlayerDisconnectMessageArgs e) {
+        // There's probably an easier anonymous function way to do this
+
+        // First find the player so we can call remove
+        foreach (Player player in m_players) {
+            if (player.IsNetPlayer(e.netPlayer)) {
+                player.Remove();
+                break;
+            }
+        }
+
+        // Now remove it from our list.
+        m_players.RemoveAll(new CheckNetPlayer(e.netPlayer).IsNetPlayer);
+    }
+
+    void Connected(object sender, EventArgs e) {
+    }
+
+    void Disconnected(object sender, EventArgs e) {
+    }
+
+    void Cleanup() {
+        if (m_server != null) {
+            m_server.Close();
+        }
+    }
+
+    void OnDestroy() {
+        Cleanup();
+    }
+
+    void OnApplicationExit() {
+        Cleanup();
+    }
+
+    void Update() {
+        // Player's should probably be GameObjecs and have a script component that is doing this
+        foreach (Player player in m_players) {
+            player.Update();
+        }
+        m_goal.Update();
+    }
+
+    public System.Random m_rand;
+    private Goal m_goal;
+    private GameServer m_server;
+    private List<Player> m_players;
+}
+
+}  // namespace HappyFunTimesExample
+
---
+++ Unity3D/src/NetPlayer.cs
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2014, Gregg Tavares.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Gregg Tavares. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+namespace HappyFunTimes {
+
+public class NetPlayer {
+
+    public delegate void TypedCmdEventHandler<T>(T eventArgs) where T : MessageCmdData;
+
+    private class CmdConverter<T> where T : MessageCmdData
+    {
+        public CmdConverter(TypedCmdEventHandler<T> handler) {
+            m_handler = handler;
+        }
+
+        public void Callback(MessageCmdData data) {
+            m_handler((T)data);
+        }
+
+        TypedCmdEventHandler<T> m_handler;
+    }
+
+    public NetPlayer(GameServer server, int id) {
+        m_server = server;
+        m_id = id;
+        m_handlers = new Dictionary<string, CmdEventHandler>();
+    }
+
+    public void RegisterCmdHandler<T>(TypedCmdEventHandler<T> callback) where T : MessageCmdData {
+        string name = MessageCmdDataNameDB.GetCmdName(typeof(T));
+        if (name == null) {
+            throw new System.InvalidOperationException("no CmdNameAttribute on " + typeof(T).Name);
+        }
+        CmdConverter<T> converter = new CmdConverter<T>(callback);
+        m_handlers[name] = converter.Callback;
+    }
+
+    private delegate void CmdEventHandler(MessageCmdData cmdData);
+
+    public void SendCmd(MessageCmdData data) { // Make it Ob's name is the message.
+        string name = MessageCmdDataNameDB.GetCmdName(data.GetType());
+        MessageCmd msgCmd = new MessageCmd(name, data);
+        m_server.SendCmd("client", m_id, msgCmd);
+    }
+
+    public void SendEvent(MessageCmd cmd) {
+        CmdEventHandler handler;
+        if (!m_handlers.TryGetValue(cmd.cmd, out handler)) {
+            Debug.LogError("unhandled NetPlayer cmd: " + cmd.cmd);
+            return;
+        }
+        handler(cmd.data);
+    }
+
+    private GameServer m_server;
+    private int m_id;
+    private Dictionary<string, CmdEventHandler> m_handlers;  // handlers by command name
+};
+
+
+}
+
