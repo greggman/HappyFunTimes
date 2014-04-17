7dee21a488da752a704ad646020b01eba0a7366a
Gregg Tavares <github@greggman.com>
2014-04-17 18:30:39 +0900

    Make input.js use Misc.getAbsolutePosition

--- public/examples/scripts/input.js
+++ public/examples/scripts/input.js
@@ -30,7 +30,7 @@
  */
 "use strict";
 
-define(function() {
+define(['./misc'], function(Misc) {
   // Provides a map from direction to various info.
   //
   // Example:
@@ -69,25 +69,9 @@ define(function() {
   };
 
   var getRelativeCoordinates = (function(window, undefined) {
-    /**
-     * Returns the absolute position of an element for certain browsers.
-     * @param {HTML Element} element The element to get a position for.
-     * @return {Object} An object containing x and y as the absolute position
-     *   of the given element.
-     */
-    var getAbsolutePosition = function(element) {
-      var r = { x: element.offsetLeft, y: element.offsetTop };
-      if (element.offsetParent) {
-        var tmp = getAbsolutePosition(element.offsetParent);
-        r.x += tmp.x;
-        r.y += tmp.y;
-      }
-      return r;
-    };
-
     return function(reference, event) {
       // Use absolute coordinates
-      var pos = getAbsolutePosition(reference);
+      var pos = Misc.getAbsolutePosition(reference);
       var x = event.pageX - pos.x;
       var y = event.pageY - pos.y;
       return { x: x, y: y };
