602721254e58ec552b2f3104b0b4069c5ffe7324
Gregg Tavares <github@greggman.com>
2014-04-19 13:42:12 +0900

    Add callback when all sounds have loaded

--- public/examples/scripts/audio.js
+++ public/examples/scripts/audio.js
@@ -4,6 +4,15 @@ define(function() {
 
   // To play a sound, simply call audio.playSound(id), where id is
   // one of the keys of the g_sound_files array, e.g. "damage".
+
+  // options:
+  //   startedOnTouchCallback: on iOS no sounds can be played unless at least one is first initiated during
+  //       a use gesture. If a function is attached here it will be called when that user gesture has happened.
+  //       This is useful for situtations like jamjam where sounds 'should' start right from the beginning
+  //       even if the player as not touched the screen. In that case we put up a message, "touch the screen"
+  //       and remove that message when we get this callback
+  //
+  //   callback: called when all the sounds have loaded.
   var AudioManager = function(sounds, options) {
     options = options || {};
     var g_context;
@@ -39,10 +48,13 @@ define(function() {
           // Decoding was successful, do something useful with the audio buffer
           that.buffer = decodedBuffer;
           if (opt_callback) {
-            opt_callback();
+            opt_callback(false);
           }
         }, function onFailure() {
            console.error("failed to decoding audio buffer: " + filename);
+           if (opt_callback) {
+             opt_callback(true);
+           }
         });
       }
       req.addEventListener("error", function(e) {
@@ -71,14 +83,19 @@ define(function() {
       for (var i = 0; i < samples; i++) {
         var audio = new Audio();
         var that = this;
-        audio.addEventListener("canplaythrough", function() {
+        var checkCallback = function(err) {
           that.waiting_on_load--;
           if (opt_callback) {
-            opt_callback();
+            opt_callback(err);
           }
+        };
+        audio.addEventListener("canplaythrough", function() {
+          checkCallback(false);
         }, false);
         audio.src = filename;
-        //audio.onerror = handleError(filename, audio);
+        audio.onerror = function() {
+          checkCallback(true);
+        };
         audio.load();
         this.audio[i] = audio;
       }
@@ -191,12 +208,26 @@ define(function() {
         g_createFn = AudioTagSound;
       }
 
+      var soundsPending = 1;
+      var soundsLoaded = function() {
+        --soundsPending;
+        if (soundsPending == 0 && options.callback) {
+          options.callback();
+        }
+      };
+
       if (sounds) {
         for (var sound in sounds) {
           var data = sounds[sound];
-          this.loadSound(sound, data.filename, data.samples)
+          ++soundsPending;
+          this.loadSound(sound, data.filename, data.samples, soundsLoaded);
         }
       }
+
+      // so that we generate a callback even if there are no sounds.
+      // That way users don't have to restructure their code if they have no sounds or if they
+      // disable sounds by passing none in.
+      setTimeout(soundsLoaded, 0);
 
       if (webAudioAPI) {
         setupGesture();
