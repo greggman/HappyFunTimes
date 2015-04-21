bff7a074ccbd10ffabf1504c9025267afc949437
Gregg Tavares <github@greggman.com>
2015-04-21 16:11:00 +0900

    change subst to have templates

--- dev/js/subst.js
+++ dev/js/subst.js
@@ -37,8 +37,130 @@ var Subst = function() {
   var replaceParamsRE = /%\(([^\)]+)\)s/g;
   var replaceHandlers = {};
 
-  this.registerReplaceHandler = function(keyword, handler) {
+  var handleString = function(data, params) {
+    return data.data;
+  }.bind(this);
+
+  var handleSubst = function(data, params) {
+    var key = data.data;
+    // If there's a colon it's a handler.
+    var colonNdx = key.indexOf(":");
+    if (colonNdx >= 0) {
+      try {
+        var args = hanson.parse("{" + key + "}");
+        var handlerName = Object.keys(args)[0];  // there should only be one key here
+        var handler = replaceHandlers[handlerName];
+        if (handler) {
+          return replaceParams(handler(args[handlerName], this, params), params);
+        }
+        console.error("unknown substition handler: " + handlerName);
+      } catch (e) {
+        console.error(e);
+        console.error("bad substitution: %(" + key + ")s");
+      }
+    } else {
+      // handle normal substitutions.
+      var keys = key.split('.');
+      for (var ii = 0; ii < params.length; ++ii) {
+        var obj = params[ii];
+        for (var jj = 0; jj < keys.length; ++jj) {
+          var k = keys[jj];
+          obj = obj[k];
+          if (obj === undefined) {
+            break;
+          }
+        }
+        if (obj !== undefined) {
+          return replaceParams(obj, params);
+        }
+      }
+    }
+    console.error("unknown key: " + key);
+    return "%(" + key + ")s";
+  }.bind(this);
+
+  function registerReplaceHandler(keyword, handler) {
     replaceHandlers[keyword] = handler;
+  };
+
+  function Template(template) {
+    this.apply = function(params) {
+      params = Array.isArray(params) ? params : [params];
+
+      var result = template.map(function(part) {
+        return part.fn(part, params);
+      });
+
+      return result.join("");
+    }
+  }
+
+  function createTemplate(str) {
+    var template = [];
+    var length = str.length;
+    var depth = 0;
+    var lastChar = '';
+    var justEnded = false;
+    var start = 0;
+    var startLine = -1;
+    var lineNum = 1;
+
+
+    function addString(start, end) {
+      template.push({ fn: handleString, data: str.substring(start, end) });
+    }
+
+    function addSubst(start, end) {
+      template.push({ fn: handleSubst,  data: str.substring(start, end) });
+    }
+
+    for (var ii = 0; ii < length; ++ii) {
+      var c = str[ii];
+      if (c === '%') {
+        if (lastChar === '%') {
+          addString(start, ii);
+          start = ii + 1;
+          c = '';  // so lastChar will not be '%'
+        }
+      } else if (c === '(') {
+        if (lastChar === '%') {
+          if (depth === 0) {
+            addString(start, ii - 1);
+            start = ii + 1;
+          }
+          ++depth;
+        }
+      } else if (c === ')') {
+        if (depth > 0) {
+          --depth;
+          if (depth < 0) {
+            throw new Error("bad template at line: " + lineNum);
+          }
+          if (depth === 0) {
+            addSubst(start, ii);
+            startLine = lineNum;
+            start = ii + 1;
+            justEnded = true;
+          }
+        }
+      } else if (lastChar === ')') {
+        if (justEnded) {
+          justEnded = false;
+          template[template.length - 1].subtype = c;
+          start += 1;
+        }
+      } else if (c === '\n') {
+        ++lineNum;
+      }
+      lastChar = c;
+    }
+
+    if (depth) {
+      throw new Error("no closing param for substitution at line: " + startLine);
+    }
+
+    addString(start, ii);
+    return new Template(template);
   };
 
   /**
@@ -52,47 +174,13 @@ var Subst = function() {
    * @param {Object|Object[]} params one or more objects.
    * @returns {string} string with replaced parts
    */
-  this.replaceParams = function(str, params) {
-    if (!params.length) {
-      params = [params];
-    }
-
-    return str.replace(replaceParamsRE, function(match, key) {
-      var colonNdx = key.indexOf(":");
-      if (colonNdx >= 0) {
-        try {
-          var args = hanson.parse("{" + key + "}");
-          var handlerName = Object.keys(args)[0];
-          var handler = replaceHandlers[handlerName];
-          if (handler) {
-            return handler(args[handlerName], params);
-          }
-          console.error("unknown substition handler: " + handlerName);
-        } catch (e) {
-          console.error(e);
-          console.error("bad substitution: %(" + key + ")s");
-        }
-      } else {
-        // handle normal substitutions.
-        var keys = key.split('.');
-        for (var ii = 0; ii < params.length; ++ii) {
-          var obj = params[ii];
-          for (var jj = 0; jj < keys.length; ++jj) {
-            var k = keys[jj];
-            obj = obj[k];
-            if (obj === undefined) {
-              break;
-            }
-          }
-          if (obj !== undefined) {
-            return obj;
-          }
-        }
-      }
-      console.error("unknown key: " + key);
-      return "%(" + key + ")s";
-    });
+  function replaceParams(str, params) {
+    return createTemplate(str).apply(params);
   };
+
+  this.replaceParams = replaceParams;
+  this.createTemplate = createTemplate;
+  this.registerReplaceHandler = registerReplaceHandler;
 };
 
 module.exports = Subst;
