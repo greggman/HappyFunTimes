9eccc48eb3e463db701df900eccb1df282665e24
Gregg Tavares <github@greggman.com>
2015-05-18 21:30:15 +0900

    update showgames for app

--- public/scripts/showgames.js
+++ public/scripts/showgames.js
@@ -33,57 +33,94 @@
 
 requirejs(
   [ 'hft/hft-system',
+    'hft/appredirector',
+    'hft/misc/cookies',
+    'hft/misc/dialog',
+    'hft/misc/misc',
     'hft/misc/strings',
   ], function(
     HFTSystem,
+    appRedirector,
+    Cookie,
+    dialog,
+    misc,
     Strings) {
 
-  var $ = function(id) {
-    return document.getElementById(id);
+  var $ = document.getElementById.bind(document);
+
+  function showGames() {
+    if (!document.hasFocus()) {
+      return;  // TODO: Put up "scan" button?
+    }
+    var debug = window.location.search.indexOf("debug") >= 0;
+    var log = debug ? console.log.bind(console) : function() {};
+
+    var gamemenu = $("gamemenu");
+    var nogames  = $("nogames");
+    var template = $("item-template").text;
+    var oldHtml = "";
+    var hftSystemDisconnected = false;
+
+    var hftSystem = new HFTSystem();
+    hftSystem.on('runningGames', function(obj) {
+      log(obj);
+      // If there's only one game just go to it.
+      if (obj.length === 1 && obj[0].controllerUrl) {
+        log("goto:", obj[0].controllerUrl);
+        if (!debug) {
+          window.location.href = obj[0].controllerUrl;
+        }
+        return;
+      }
+
+      var items = [];
+      for (var ii = 0; ii < obj.length; ++ii) {
+        var game = obj[ii];
+        var runtimeInfo = game.runtimeInfo;
+        if (runtimeInfo) {
+          var hftInfo = runtimeInfo.info.happyFunTimes;
+          // Not sure how I should figure out the name and screenshot.
+          var dev = (runtimeInfo.originalGameId !== hftInfo.gameId) ? "(*)" : "";
+          game.name = dev + (hftInfo.name || runtimeInfo.originalGameId);
+          game.screenshotUrl = game.screenshotUrl;
+          items.push(Strings.replaceParams(template, game));
+        }
+      }
+      var html = items.join("");
+      if (html !== oldHtml) {
+        oldHtml = html;
+        gamemenu.innerHTML = html;
+      }
+
+      nogames.style.display = items.length ? "none" : "block";
+
+    });
+    hftSystem.on('disconnect', function() {
+      // if the HFT system disconnected it means
+      // HFT is not or is no longer running. Since we might be
+      // doing DEV we don't want to leave but if
+      // it's a player give them the option to restart
+      if (!hftSystemDisconnected) {
+        hftSystemDisconnected = true;
+        dialog.modal({
+          title: "HappyFunTimes",
+          msg: "Touch To Restart",
+        }, function() {
+          var redirCookie = new Cookie("redir");
+          var url = redirCookie.get() || "http://happyfuntimes.net";
+          console.log("goto: " + url);
+          window.location.href = url;
+        });
+      }
+    });
   };
 
-  var debug = window.location.search.indexOf("debug") >= 0;
-  var log = debug ? console.log.bind(console) : function() {};
-
-  var gamemenu = $("gamemenu");
-  var nogames  = $("nogames");
-  var template = $("item-template").text;
-  var oldHtml = "";
-
-  var hftSystem = new HFTSystem();
-  hftSystem.on('runningGames', function(obj) {
-    log(obj);
-    // If there's only one game just go to it.
-    if (obj.length === 1 && obj[0].controllerUrl) {
-      log("goto:", obj[0].controllerUrl);
-      if (!debug) {
-        window.location.href = obj[0].controllerUrl;
-      }
-      return;
-    }
-
-    var items = [];
-    for (var ii = 0; ii < obj.length; ++ii) {
-      var game = obj[ii];
-      var runtimeInfo = game.runtimeInfo;
-      if (runtimeInfo) {
-        var hftInfo = runtimeInfo.info.happyFunTimes;
-        // Not sure how I should figure out the name and screenshot.
-        var dev = (runtimeInfo.originalGameId !== hftInfo.gameId) ? "(*)" : "";
-        game.name = dev + (hftInfo.name || runtimeInfo.originalGameId);
-        game.screenshotUrl = game.screenshotUrl;
-        items.push(Strings.replaceParams(template, game));
-      }
-    }
-    var html = items.join("");
-    if (html !== oldHtml) {
-      oldHtml = html;
-      gamemenu.innerHTML = html;
-    }
-
-    nogames.style.display = items.length ? "none" : "block";
-
+  appRedirector.checkForApp({
+    inApp: showGames,
+    notInApp: showGames,
+    setCookies: false,
   });
+  //showGames();
 });
 
 
