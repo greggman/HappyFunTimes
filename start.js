d1536f12ed8e44369b582ff0e6c2696b4b80710b
Gregg Tavares <github@greggman.com>
2016-11-19 20:26:50 +0900

    handle standalone server

--- start.js
+++ start.js
@@ -1,5 +1,24 @@
-const inMainProcess = (process && process.type === 'browser');
-module.exports = inMainProcess ? require('./server/server') : require('./dist/hft');
+
+function getAPIForSituation() {
+  const isBrowserProcess = (process && process.type === 'browser');
+  const isMain = (require && require.main === module);
+
+  if (isBrowserProcess) {
+    // For the browser process in electron
+    return require('./server/server');
+  }
+
+  if (isMain) {
+    // If run from node directly
+    return require('./server/standalone');
+  }
+
+  // For a webpage in electron or other
+  return require('./dist/hft');
+}
+
+module.exports = getAPIForSituation();
+
 
 
 
